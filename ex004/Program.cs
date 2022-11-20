766da1acd9956bf2b889697d720b644c6d2f019e
HafiAga <ulguiorra2@gmail.com>
2022-11-20 17:52:29 +0300

    Ex004

---
+++ ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "зулфизар")
+{
+    Console.WriteLine("Ура, это же ЗУЛФИЗАР!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
